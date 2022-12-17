ea8f1304ca366893ce1718fa6749d69030136335
Prashant Gupta <meengineerprashant@gmail.com>
2022-12-17 21:48:09 +0000

    cpmmit

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ import hmsRouter from './configurations/routes/hmsRoutes';
 config();
 const app = express();
 
-
 // Middlewares
 app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(express.json());
 
@@ -25,7 +24,6 @@ global.yekolaLogger = winstonLogger;
 
 app.use("/api/v1", router);
 app.use("/api/v1", hmsRouter);
-
 
 const PORT = process.env.APP_PORT || 5001;
 app.listen(PORT, () => console.log(`⚡Server is running on PORT: ${PORT} `));
