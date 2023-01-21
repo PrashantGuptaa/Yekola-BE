a4c617cb9848432c71ba32fb75f7912e36b317be
PrashantGuptaa <meengineerprashant@gmail.com>
2023-01-22 04:21:49 +0530

    Removed Unused code

--- utils/utils.js
+++ utils/utils.js
@@ -14,35 +14,17 @@ export const extractUsefulRoomInformation = (roomObj) => {
   updatedName = updatedName.replace(/_/g, ' ');
   return {
     roomId,
-    // name: getRoomName(name),
     name: updatedName,
     description,
     createdBy,
     instructor,
     loading: false,
-    disabled: isRoomDisabled(startDateTime, endDateTime),
     startDateTime,
     endDateTime,
-    // dateStr,
-    // timeStr,
-    // date: date && JSON.parse(date),
-    // time: time && JSON.parse(time),
+
   };
 };
 
-// export const getRoomName = (roomName) => {
-//     const roomNameArr = roomName.split("_");
-//     const str = roomNameArr.join(" ").slice(0, roomNameArr.length - 2);
-//     return str.substring(0, str.lastIndexOf(" "));
-// }
 
-export const replaceAllfromString = (str, search, replace) => {
-  while (str.includes(search)) {
-    console.log("============= Updating Str ===============", str, search, replace);
-    str.replace(search, replace);
-  }
-  return str;
-};
 
-const isRoomDisabled = (start, end) => moment().isBetween(start, end, 'm');
 
