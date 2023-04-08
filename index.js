d49deab1c656732baad4c796ce1d57cca1c58f31
Prashant Gupta <meengineerprashant@gmail.com>
2023-04-08 12:21:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-import express from 'express';
-import { config } from 'dotenv';
-import cors from 'cors';
-import router from './configurations/routes/routes';
-import winstonLogger from './utils/logger.js';
-import bodyParser from 'body-parser';
-import hmsRouter from './configurations/routes/hmsRoutes';
-import './Model/sequelize';
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import router from "./configurations/routes/routes.js";
+import winstonLogger from "./utils/logger.js";
+import bodyParser from "body-parser";
+import hmsRouter from "./configurations/routes/hmsRoutes.js";
+import "./Model/sequelize.js";
 
 config();
 const app = express();
 
-
 // Middlewares
 app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(express.json());
 
@@ -26,7 +25,6 @@ global.yekolaLogger = winstonLogger;
 
 app.use("/api/v1", router);
 app.use("/api/v1", hmsRouter);
-
 
 const PORT = process.env.APP_PORT || 5001;
 app.listen(PORT, () => console.log(`⚡Server is running on PORT: ${PORT} `));
