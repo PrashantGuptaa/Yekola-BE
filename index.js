eda1c48ae7fa823fead6b72d21a0e66cd0399257
PrashantGuptaa <meengineerprashant@gmail.com>
2023-10-09 20:28:28 +0530

    added body parser package

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-// import express from "express";
-// import { config } from "dotenv";
-// import cors from "cors";
-// import router from "./configurations/routes/routes.js";
-// import winstonLogger from "./utils/logger.js";
-// import bodyParser from "body-parser";
-// import hmsRouter from "./configurations/routes/hmsRoutes.js";
-// import healthTestController from "./controllers/health.controller.js";
-// import './Model/connection.js';
-
-// config();
-// const app = express();
-
-// // Middlewares
-// app.use(cors());
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // parse application/json
-// app.use(bodyParser.json());
-
-// app.use(express.json());
-
-// // Global Logger
-// global.yekolaLogger = winstonLogger;
-
-// app.use("/api/v1", router);
-// app.use("/api/v1", hmsRouter);
-// app.get('/', healthTestController);
-
-// const PORT = process.env.APP_PORT || 5001;
-// app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}⚡ `));
-
-
 // app.js
 const express = require("express");
 const cors = require("cors");
@@ -47,13 +13,14 @@ const app = express();
 const routes = require("./routes");
 const {logger} = require("./services/logService");
 const { v4 } = require("uuid");
+const bodyParser = require("body-parser");
 
 const port = process.env.APP_PORT;
 const mongoUrl = process.env.MONGO_CONNECTION_STRING;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 
 app.use(httpContext.middleware);
 
