04a3f9ad267bb1cb721ae0d6744cf20bc38efa20
PrashantGuptaa <meengineerprashant@gmail.com>
2023-11-18 22:48:16 +0530

    test commit

--- services/s3Service.js
+++ services/s3Service.js
@@ -27,32 +27,33 @@ const uploadToS3 = async (Key, Body) => {
 };
 
 const readFromS3 = async (Key) => {
-    return new Promise((resolve, reject) => {
-      s3.getObject(
-        {
-          Bucket: `${process.env.S3_BUCKET}/profile`,
-          Key,
-        },
-        (err, data) => {
-          if (err) {
-            reject(err); // Reject the promise if there's an error
-          } else {
-            resolve(data.Body); // Resolve with the S3 object location if successful
-          }
+  return new Promise((resolve, reject) => {
+    s3.getObject(
+      {
+        Bucket: `${process.env.S3_BUCKET}/profile`,
+        Key,
+      },
+      (err, data) => {
+        if (err) {
+          reject(err); // Reject the promise if there's an error
+        } else {
+          resolve(data.Body); // Resolve with the S3 object location if successful
         }
-      );
-    });
-  };
-  
-  const readFromS3Service = async (Key) => {
-    try {
-        const result = await readFromS3(Key);
-        return result;
-    } catch (e) {
-        logger.error(e.message);
-        return '';
-    }  }
+      }
+    );
+  });
+};
+
+const readFromS3Service = async (Key) => {
+  try {
+    const result = await readFromS3(Key);
+    return result;
+  } catch (e) {
+    logger.error(e.message);
+    return "";
+  }
+};
 module.exports = {
   uploadToS3,
-  readFromS3Service
+  readFromS3Service,
 };
